d74835a119d6c207c3e2ae7b35a58ac793aa366d
DunamisDon <diwauripod@gmail.com>
2024-11-19 23:35:25 +0200

    added UserProfile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,6 +5,7 @@ import WelcomeMessage from './components/welcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile'
 import './App.css'
 
 function App() {
